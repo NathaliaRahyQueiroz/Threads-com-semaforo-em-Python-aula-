78151ac442e4c4896e1f539ca771df9218349fea
Nathália Rahy Queiroz <nathaliaryqz28@gmail.com>
2026-06-04 11:34:10 -0300

    Add files via upload

--- Semaforo2.2.py
+++ Semaforo2.2.py
@@ -2,70 +2,66 @@ import multiprocessing
 import time 
 import random 
 
-semaforo_pista = None
+semaforo_norte = None
+semaforo_sul = None
 semaforo_decolagem = None
-norte: int =0
-sul: int = 0
 
-def init(sp, sd, n, s):
-    global semaforo_pista
+
+def init(sn, ss, sd):
+    global semaforo_norte
+    global semaforo_sul
     global semaforo_decolagem
-    global norte
-    global sul 
+ 
 
-    semaforo_pista = sp
+    semaforo_norte = sn
+    semaforo_sul = ss
     semaforo_decolagem = sd
-    norte = n
-    sul = s
-
+  
 def decolar(pista, aviao):
-    global semaforo_pista
+    global semaforo_norte
+    global semaforo_sul
     global semaforo_decolagem
-    global norte
-    global sul 
-
-
-    with semaforo_pista:
-        if pista == 0:
-            norte.value +=1
-        else:
-            sul.value +=1
 
     with semaforo_decolagem:
-        manobrar(pista, aviao)
-        taxiar(pista, aviao)
-        decolagem(pista, aviao)
-        afastamento (pista, aviao)
-        if pista == 0:
-            norte.value = 0
+
+        if pista ==0:
+            with semaforo_norte:
+                manobrar(pista, aviao)
+                taxiar(pista, aviao)
+                decolagem(pista, aviao)
+                afastamento (pista, aviao)
+
+                
+
         else:
-            sul.value = 0
+            with semaforo_sul:
+                manobrar(pista, aviao)
+                taxiar(pista, aviao)
+                decolagem(pista, aviao)
+                afastamento (pista, aviao)
+
+                
 
           
 
-
-
 def manobrar(pista, aviao):
-    time.sleep((random.randint(3, 7))/1000)
+    time.sleep((random.randint(3, 7))/10)
 def taxiar(pista, aviao):
-    time.sleep((random.randint(5, 10))/1000)
+    time.sleep((random.randint(5, 10))/10)
 def decolagem(pista, aviao):
-    time.sleep((random.randint(6, 8))/1000)
+    time.sleep((random.randint(6, 8))/10)
 def afastamento(pista, aviao):
-    time.sleep((random.randint(3, 8))/1000)
+    time.sleep((random.randint(3, 8))/10)
 
 
 
 
 
 def main():
-    sem_pista = None
+    sem_norte = None
+    sem_sul = None
     sem_decolagem = None
-    p_norte: int = 0
-    p_sul: int = 0
-
-    p_norte= multiprocessing.Value('i', 0)
-    p_sul= multiprocessing.Value('i', 0)
+   
 
     params: int = [(0,0)]* 12
 
@@ -73,9 +69,10 @@ def main():
         params[i] = ((random.randint(0,1)), (i+1))
 
     with multiprocessing.Manager() as manager:
-        sem_pista = manager.Semaphore(1)
+        sem_norte = manager.Semaphore(1)
+        sem_sul = manager.Semaphore(1)
         sem_decolagem = manager.Semaphore(2)
-        with multiprocessing.Pool(processes=12, initializer=init, initargs=(sem_pista, sem_decolagem, p_norte, p_sul )) as pool:
+        with multiprocessing.Pool(processes=12, initializer=init, initargs=(sem_norte, sem_sul, sem_decolagem)) as pool:
             pool.starmap(decolar,params)
 
 if __name__ == '__main__':
